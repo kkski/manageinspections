dacd71c1542e737bd84726de397c9aea7a514315
kkski <akinaripl@gmail.com>
2022-04-07 22:59:34 +0200

    security

--- src/main/java/pl/coderslab/manageinspections/web/LoginController.java
+++ src/main/java/pl/coderslab/manageinspections/web/LoginController.java
@@ -3,10 +3,16 @@ package pl.coderslab.manageinspections.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.manageinspections.model.User;
+import pl.coderslab.manageinspections.repository.UserRepository;
 
 @Controller
 @RequestMapping("/")
 public class LoginController {
+
+    UserRepository userRepository;
+
     @GetMapping("/login")
     public String login() {
         return "/admin/login";
@@ -14,5 +20,16 @@ public class LoginController {
     @GetMapping("/app")
     public String viewIndex() {
         return "/app/index"; }
+
+    @GetMapping("/create-user")
+    @ResponseBody
+    public String createUser() {
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword("user");
+        userRepository.save(user);
+        return "dodano";
+    }
+
 
 }
