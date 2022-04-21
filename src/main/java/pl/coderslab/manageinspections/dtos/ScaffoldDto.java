0585dfb89d37c62a657edafac223a43d9ccb4008
kkski <akinaripl@gmail.com>
2022-04-21 05:16:16 +0200

    Validations brought back

--- src/main/java/pl/coderslab/manageinspections/dtos/ScaffoldDto.java
+++ src/main/java/pl/coderslab/manageinspections/dtos/ScaffoldDto.java
@@ -37,19 +37,21 @@ public class ScaffoldDto {
 
     @Min(value = 1, message = "Choose a difficulty and complexity category from 1 to 4")
     @Max(value = 4, message = "Choose a difficulty and complexity category from 1 to 4")
-    @NotNull
-    @NotEmpty(message = "You must choose a grade of scaffolding.")
+    @NotNull(message = "You must choose a grade of scaffolding.")
+    @Column(nullable = false)
     private int scaffoldGrade = 4;
 
     private String area;
 
-    @PastOrPresent(message ="Wrong date chosen.")
+    //@PastOrPresent(message ="Wrong date chosen.")
     @NotNull(message = "You must choose a date of finished erection of scaffolding.")
     @NotEmpty(message = "You must choose a date of finished erection of scaffolding.")
     private String dateOfErection;
 
-    @Size(min = 5)
-    @Column(unique = true)
+
+    @Size(min = 5, message = "Use minimum 5 characters.")
+    @NotNull
+    @NotEmpty(message = "You must choose an identification sequence of scaffold.")
     private String scaffoldId;
 
 
